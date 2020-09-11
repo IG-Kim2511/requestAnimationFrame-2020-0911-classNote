4afa255c8094ef9f9aada55781139967f24bfb5b
IG Kim <59094101+IanKim2511@users.noreply.github.com>
2020-09-11 16:55:59 -0400

    ..

---
+++ script.js
@@ -0,0 +1,22 @@
+const ilbuni = document.querySelector(".ilbuni");
+const value = document.querySelector(".value");
+let yPos = 0;
+let rafId;
+
+function render() {
+  value.innerHTML = yPos;
+  ilbuni.style.transform = `translateY(${-yPos}px)`;
+  yPos += 10;
+
+  rafId = requestAnimationFrame(render);
+
+  if (yPos > 500) {
+    cancelAnimationFrame(rafId);
+  }
+}
+
+render();
+
+window.addEventListener("click", () => {
+  cancelAnimationFrame(rafId);
+});
